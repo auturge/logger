667044d587634b5d4fa8003a6abd264d89793e1b
Curt Kaler <curt.kaler@gmail.com>
2021-05-07 15:29:24 -0700

    LogManager spec

--- src/logging/LogManager.ts
+++ src/logging/LogManager.ts
@@ -13,42 +13,39 @@ export class LogManagerClass<TEntry extends ILogEntry, TLog extends ILog<TLog, T
             throw new Error(getNullOrUndefinedErrorMessage('builder'));
 
         this.initialize = builder;
-        this.initialize.logCreated.subscribe((log) => this.onLogCreated(log));
+        this.initialize.logCreated.subscribe(this.onLogCreated);
     }
 
     /** A builder for generating new ILog channels. */
     public readonly initialize: LogBuilder<TEntry, TLog>;
 
     /** Disables all channels on the log. */
-    public disable(log: TLog): ILog<TLog, TEntry>;
-    public disable(logName: string): ILog<TLog, TEntry>;
-    public disable(logOrName: ILog<TLog, TEntry> | string): ILog<TLog, TEntry> {
-        throwIfNullOrUndefined(logOrName, 'logOrName');
-        var log = typeof logOrName === "string" ? this.getLog(logOrName) : logOrName;
+
+    public disable(logName: string): ILog<TLog, TEntry> {
+        throwIfNullOrEmpty(logName, 'logName');
+        var log = this.getLog(logName);
 
         if (log == null)
-            throw new Error(`No log exists with the name ${ logOrName }.`)
+            throw new Error(`No log exists with the name ${ logName }.`)
 
         log.enabled = false;
         return log;
     };
 
     /** Enables all channels on the log. */
-    public enable(log: TLog): ILog<TLog, TEntry>;
-    public enable(logName: string): ILog<TLog, TEntry>;
-    public enable(logOrName: ILog<TLog, TEntry> | string): ILog<TLog, TEntry> {
-        throwIfNullOrUndefined(logOrName, 'logOrName');
-        var log = typeof logOrName === "string" ? this.getLog(logOrName) : logOrName;
+    public enable(logName: string): ILog<TLog, TEntry> {
+        throwIfNullOrEmpty(logName, 'logName');
+        var log = this.getLog(logName);
 
         if (log == null)
-            throw new Error(`No log exists with the name ${ logOrName }.`)
+            throw new Error(`No log exists with the name ${ logName }.`)
 
         log.enabled = true;
         return log;
     };
 
     public getLog(logName: string): ILog<TLog, TEntry> | null {
-        throwIfNullOrEmpty(logName, 'logOrName');
+        throwIfNullOrEmpty(logName, 'logName');
 
         const index = this._logNames.indexOf(logName);
         if (index == -1)
@@ -66,20 +63,12 @@ export class LogManagerClass<TEntry extends ILogEntry, TLog extends ILog<TLog, T
         // search for the index in the NAMES array,
         // which will be faster than searching the 'primary' array of logs.
         const index = this._logNames.indexOf(log.name);
+
         if (index != -1) {
-            this._logNames.slice[ index ];
-            this._logs.slice[ index ];
+            this._logNames.splice(index, 1);
+            this._logs.splice(index, 1);
         }
         this._logNames.push(log.name);
         this._logs.push(log);
     };
-
-    // private onBuilderCreated(builder: LogBuilder<TEntry, TLog>): void {
-    //     throwIfNullOrUndefined(builder, 'builder');
-    //     const index = this._logNames.indexOf(builder.name);
-    //     if (index != -1) {
-    //         this._builders[ index ];
-    //     }
-    //     this._builders.push(builder);
-    // };
 }
---
+++ test/unit/logging/LogManager.spec.ts
@@ -0,0 +1,236 @@
+import { assert } from 'chai';
+import { AnyRandom } from '@auturge/testing';
+import { TEST_CHANNEL_1, TEST_CHANNEL_2 } from '@test/objects';
+
+import { StatusLog } from '@src/logging/StatusLog/StatusLog';
+import { IChannel, LogManagerClass } from '@src/logging';
+import { IStatusEntry } from '@src/logging/StatusLog/IStatusEntry';
+import { StatusLogBuilder } from '@src/logging/StatusLog/StatusLogBuilder';
+
+describe('LogManager', () => {
+
+    var builder, manager;
+    var logName;
+
+    function getTestLogger(name: string, channels: IChannel<IStatusEntry>[] = []) {
+        if (!channels.length) {
+            channels.push(TEST_CHANNEL_1);
+        }
+
+        return new StatusLog(name, channels);
+    }
+
+    function setupTestSuite() {
+        logName = AnyRandom.string(5, 8);
+        builder = new StatusLogBuilder(logName);
+
+        manager = new LogManagerClass(builder);
+    }
+
+    function tearDownSuite() {
+    }
+
+    describe('ctor', () => {
+
+        beforeEach(setupTestSuite);
+        beforeEach(tearDownSuite);
+
+        [
+            { key: 'null', value: null },
+            { key: 'undefined', value: undefined },
+        ].forEach(({ key, value }) => {
+            it(`ctor - throws when 'builder' parameter is ${ key }`, () => {
+                const builder = <any>value;
+
+                assert.throws(() => {
+                    new LogManagerClass(builder);
+                });
+            });
+        });
+
+        it(`ctor - sets values appropriately`, () => {
+
+            manager = new LogManagerClass(builder);
+
+            assert.deepEqual(manager.initialize, builder);
+            assert.equal(manager.initialize.logCreated.handlers[ 0 ], manager.onLogCreated);
+        });
+    });
+
+    describe('disable', () => {
+        beforeEach(setupTestSuite);
+        beforeEach(tearDownSuite);
+
+        [
+            { key: 'null', value: null },
+            { key: 'undefined', value: undefined },
+            { key: 'empty string', value: '' },
+        ].forEach(({ key, value }) => {
+            it(`disable - throws when 'logName' parameter is ${ key }`, () => {
+                const logName = value;
+
+                assert.throws(() => {
+                    manager.disable(logName);
+                });
+            });
+        });
+
+        it('disable - throws when no such log exists', () => {
+            const name = AnyRandom.string(5, 8);
+            manager[ '_logNames' ] = [];
+            manager[ '_logs' ] = [];
+
+            assert.throws(() => {
+                manager.disable(name);
+            });
+        });
+
+        it('disable - takes a log name and disables the corresponding log', () => {
+            const name = AnyRandom.string(5, 8);
+            const logger = getTestLogger(name);
+            logger.enabled = true;
+            manager[ '_logNames' ] = [ name ];
+            manager[ '_logs' ] = [ logger ];
+
+            manager.disable(name);
+
+            assert.equal(logger.enabled, false);
+        });
+
+
+    });
+
+    describe('enable', () => {
+        beforeEach(setupTestSuite);
+        beforeEach(tearDownSuite);
+
+        [
+            { key: 'null', value: null },
+            { key: 'undefined', value: undefined },
+            { key: 'empty string', value: '' },
+        ].forEach(({ key, value }) => {
+            it(`enable - throws when 'logName' parameter is ${ key }`, () => {
+                const logName = value;
+
+                assert.throws(() => {
+                    manager.enable(logName);
+                });
+            });
+        });
+
+        it('enable - throws when no such log exists', () => {
+            const name = AnyRandom.string(5, 8);
+            manager[ '_logNames' ] = [];
+            manager[ '_logs' ] = [];
+
+            assert.throws(() => {
+                manager.enable(name);
+            });
+        });
+
+        it('enable - takes a log name and enables the corresponding log', () => {
+            const name = AnyRandom.string(5, 8);
+            const logger = getTestLogger(name);
+            logger.enabled = false;
+            manager[ '_logNames' ] = [ name ];
+            manager[ '_logs' ] = [ logger ];
+
+            manager.enable(name);
+
+            assert.equal(logger.enabled, true);
+        });
+
+
+    });
+
+    describe('getLog', () => {
+        beforeEach(setupTestSuite);
+        beforeEach(tearDownSuite);
+
+        [
+            { key: 'null', value: null },
+            { key: 'undefined', value: undefined },
+            { key: 'empty string', value: '' },
+        ].forEach(({ key, value }) => {
+            it(`getLog - throws when 'logName' parameter is ${ key }`, () => {
+                const logName = value;
+
+                assert.throws(() => {
+                    manager.getLog(logName);
+                });
+            });
+        });
+
+        it('getLog - returns null when no such log exists', () => {
+            const name = AnyRandom.string(5, 8);
+            manager[ '_logNames' ] = [];
+            manager[ '_logs' ] = [];
+
+            const result = manager.getLog(name);
+
+            assert.isNull(result);
+        });
+
+        it('getLog - takes a log name and returns the corresponding log', () => {
+            const name = AnyRandom.string(5, 8);
+            const logger = getTestLogger(name);
+            manager[ '_logNames' ] = [ name ];
+            manager[ '_logs' ] = [ logger ];
+
+            const result = manager.getLog(name);
+
+            assert.deepEqual(result, logger);
+        });
+    });
+
+    describe('onLogCreated', () => {
+        beforeEach(setupTestSuite);
+        beforeEach(tearDownSuite);
+
+        [
+            { key: 'null', value: null },
+            { key: 'undefined', value: undefined },
+        ].forEach(({ key, value }) => {
+            it(`onLogCreated - throws when 'log' parameter is ${ key }`, () => {
+                const log = value;
+
+                assert.throws(() => {
+                    manager.onLogCreated(log);
+                });
+            });
+        });
+
+        it('onLogCreated - adds the log name to one array, adds the log to the second array', () => {
+            const alice = getTestLogger('alice');
+            const bob = getTestLogger('bob');
+            const carol = getTestLogger('carol');
+            manager[ '_logNames' ] = [ alice.name, bob.name ];
+            manager[ '_logs' ] = [ alice, bob ];
+
+            manager[ 'onLogCreated' ](carol);
+
+            assert.deepEqual(manager[ '_logNames' ],
+                [ alice.name, bob.name, carol.name ]);
+            assert.deepEqual(manager[ '_logs' ],
+                [ alice, bob, carol ]);
+        });
+
+        it('onLogCreated - replaces an existing log with the same name', () => {
+            const alice = getTestLogger('alice');
+            const bob1 = getTestLogger('bob', [ TEST_CHANNEL_1 ]);
+            const bob2 = getTestLogger('bob', [ TEST_CHANNEL_2 ]);
+            const carol = getTestLogger('carol');
+            manager[ '_logNames' ] = [ alice.name, bob1.name, carol.name ];
+            manager[ '_logs' ] = [ alice, bob1, carol ];
+
+            manager[ 'onLogCreated' ](bob2);
+
+            assert.deepEqual(manager[ '_logNames' ],
+                [ alice.name, carol.name, bob2.name ]);
+            assert.deepEqual(manager[ '_logs' ],
+                [ alice, carol, bob2 ]);
+        });
+
+    });
+
+});
